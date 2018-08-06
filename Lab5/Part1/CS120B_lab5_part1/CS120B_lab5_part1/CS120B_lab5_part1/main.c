d78a0f5c8f0288690b7d45954c804a6b2aaf9224
kconn006 <kconn006@ucr.edu>
2018-08-06 13:24:50 -0700

    lab4_part1

---
+++ Lab5/Part1/CS120B_lab5_part1/CS120B_lab5_part1/CS120B_lab5_part1/main.c
@@ -0,0 +1,18 @@
+/*
+ * CS120B_lab5_part1.c
+ *
+ * Created: 8/6/2018 12:54:24 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
