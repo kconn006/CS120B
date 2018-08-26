b2a49db069bdda1c8c5975137faa92f7061619f3
kconn006 <kconn006@ucr.edu>
2018-08-25 19:36:57 -0700

    Part 3 done

---
+++ Lab_LED/Part3/Lab_LED_part3/Lab_LED_part3/Debug/main.d
@@ -0,0 +1,40 @@
+main.d main.o: .././main.c \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ C:\Users\Keilani\Downloads\EE120B\7.0\Packs\atmel\ATmega_DFP\1.2.209\include/avr/iom1284p.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+C:\Users\Keilani\Downloads\EE120B\7.0\Packs\atmel\ATmega_DFP\1.2.209\include/avr/iom1284p.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
