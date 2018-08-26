3ac7f84cd6739368d4ec15629d626d2a337d14c2
kconn006 <kconn006@ucr.edu>
2018-08-26 11:28:22 -0700

    Part2 Fixed

---
+++ Lab10/Part2/Lab10_part2/Part2/Lab10_part2/Lab10_part2/Debug/main.d
@@ -0,0 +1,70 @@
+main.d main.o: .././main.c \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ C:\Users\Keilani\Downloads\EE120B\7.0\Packs\atmel\ATmega_DFP\1.2.209\include/avr/iom1284.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ ../../../../Part1/includes/keypad.h ../../../../Part1/includes/bit.h \
+ ../../../../Part1/includes/timer.h \
+ ../../../../Part1/includes/scheduler.h ../../../../Part1/includes/io.h \
+ ../../../../Part1/includes/lcd_io.c \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ ../../../../Part1/includes/io.h ../../../../Part1/includes/queue.h \
+ c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+C:\Users\Keilani\Downloads\EE120B\7.0\Packs\atmel\ATmega_DFP\1.2.209\include/avr/iom1284.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+../../../../Part1/includes/keypad.h:
+
+../../../../Part1/includes/bit.h:
+
+../../../../Part1/includes/timer.h:
+
+../../../../Part1/includes/scheduler.h:
+
+../../../../Part1/includes/io.h:
+
+../../../../Part1/includes/lcd_io.c:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+../../../../Part1/includes/io.h:
+
+../../../../Part1/includes/queue.h:
+
+c:\users\keilani\downloads\ee120b\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
